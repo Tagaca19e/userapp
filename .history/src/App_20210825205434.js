fca21d44e381b3814bccd7d3c57e18f9311d18bb
Eidmone <eidmonetagaca@gmail.com>
2021-08-25 20:56:53 -0700

    final

--- .history/src/App_20210825205434.js
+++ .history/src/App_20210825205434.js
@@ -22,30 +22,6 @@ function App() {
       setloading(false);
     }, 1500);
   }, []);
-  const [theme, setTheme] = useState(getStorageTheme());
-  const { setChartColor, setChartFontColor } = useContext(GithubContext);
-  const toggleTheme = () => {
-    if (theme === "light-theme") {
-      setTheme("dark-theme");
-      setChartColor("#1a1a1a");
-      setChartFontColor("#d6d6d6");
-    } else {
-      setTheme("light-theme");
-      setChartColor("#ffffff");
-      setChartFontColor("#4f4f4f");
-    }
-  };
-  useEffect(() => {
-    document.documentElement.className = theme;
-    sessionStorage.setItem("theme", theme);
-    if (theme === "light-theme") {
-      setChartColor("#ffffff");
-      setChartFontColor("#4f4f4f");
-    } else {
-      setChartColor("#1a1a1a");
-      setChartFontColor("#d6d6d6");
-    }
-  }, [theme]);
 
   // const toggleTheme = () => {
   //   if (theme === 'light-theme') {
@@ -84,7 +60,7 @@ function App() {
         <Router>
           <Switch>
             <Route path="/" exact={true}>
-              <Dashboard toggleTheme={toggleTheme}></Dashboard>
+              <Dashboard></Dashboard>
             </Route>
             <Route path="*">
               <Error />
